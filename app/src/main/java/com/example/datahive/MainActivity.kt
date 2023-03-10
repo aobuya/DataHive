e11389eae48c4795930576e9be9e8c563be05046
slowburn-404 <borisochola32@gmail.com>
2023-03-10 22:09:48 +0300

    splash screen

---
+++ app/src/main/java/com/example/datahive/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.datahive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.datahive.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            val view = binding.root
+
+            super.onCreate(savedInstanceState)
+            setContentView(view)
+
+            supportActionBar?.hide()
+    }
+}
