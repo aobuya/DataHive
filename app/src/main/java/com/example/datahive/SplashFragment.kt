b9d4368f51308e7d259602bbe68a6e8f9cee33f2
aobuya <outlookpc1>
2023-03-20 11:40:57 +0300

    Add fix for recycler view

--- app/src/main/java/com/example/datahive/SplashFragment.kt
+++ app/src/main/java/com/example/datahive/SplashFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.datahive.databinding.FragmentSplashBinding
+import com.example.datahive.holder.MainNavigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
@@ -19,6 +20,8 @@ class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+    private lateinit var dataHiveAuth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -27,16 +30,28 @@ class SplashFragment : Fragment() {
 
 
         Handler(Looper.getMainLooper()).postDelayed({
-                if (finishedOnBoarding()) {
+            dataHiveAuth = FirebaseAuth.getInstance()
+
+            val isUserLoggedIn = dataHiveAuth.currentUser
+
+            if (finishedOnBoarding()) {
+                if (isUserLoggedIn != null) {
+                    requireActivity().run {
+                        startActivity(Intent(this, MainNavigation::class.java))
+                        finish()
+                    }
+                }else{
                     findNavController().navigate(R.id.action_viewPager_to_LobbyFragment)
-                } else {
-                    findNavController().navigate(R.id.action_splashFragment_to_viewPager)
 
                 }
-            }, 2000)
-            return binding.root
+            } else {
+                findNavController().navigate(R.id.action_splashFragment_to_viewPager)
 
-        }
+            }
+        }, 2000)
+        return binding.root
+
+    }
 
 
     private fun finishedOnBoarding(): Boolean {
--- app/src/main/java/com/example/datahive/login/ResetPassword.kt
+++ app/src/main/java/com/example/datahive/login/ResetPassword.kt
@@ -1,11 +1,14 @@
 package com.example.datahive.login
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.datahive.databinding.ActivityResetPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class ResetPassword : AppCompatActivity() {
     private lateinit var binding : ActivityResetPasswordBinding
@@ -17,24 +20,48 @@ class ResetPassword : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        dataHiveAuth = FirebaseAuth.getInstance()
+        val getUser = dataHiveAuth.currentUser
+
 
 
         binding.btnResetPassword.setOnClickListener{
+            val email = binding.resetEmail.text.trim().toString().trim()
 
-            //resetPassword()
-            Toast.makeText(this,"Password sent, check your inbox",Toast.LENGTH_SHORT).show()
+            resetPassword(email, getUser)
         }
 
         //redirect users to the login screen
         binding.loginRedirect.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+            finish()
         }
 
 
     }
     //Firebase password rest implementation
-    private fun resetPassword() {
-        TODO("Not yet implemented")
+    private fun resetPassword(email: String, user: FirebaseUser?) {
+
+        if (email.isNotEmpty()) {
+            dataHiveAuth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Email sent")
+                    Toast.makeText(
+                        this,
+                        "Password sent, check your email inbox",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    val intent = Intent(this, SignUpActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Account does not exist", Toast.LENGTH_LONG).show()
+                }
+            }
+        }else {
+            Toast.makeText(this, "Please enter you email", Toast.LENGTH_SHORT).show()
+        }
     }
 }
